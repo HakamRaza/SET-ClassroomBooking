1b4ea4d857cf6da8fade8bf1f5b7e70befe40351
HakamRaza <ismatcd@yahoo.com>
2022-09-12 19:58:46 +0800

    Validation

--- tests/Feature/Teacher/TeacherTest.php
+++ tests/Feature/Teacher/TeacherTest.php
@@ -2,15 +2,55 @@
 
 namespace Tests\Feature\Teacher;
 
+use App\Http\Requests\AddTeacherRequest;
 use App\Models\ClassroomType;
 use App\Models\Teacher;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Validator;
 use Tests\TestCase;
 
 class TeacherTest extends TestCase
 {
+    public function checkInput($payload):bool
+    {
+        return Validator::make(
+            // your payload array
+            $payload, 
+            //extract rules array from class
+            (new AddTeacherRequest())->rules()
+            // app()->call([AddTeacherRequest::class, 'rules']), 
+        )
+        // stop on first rule fail method
+        ->stopOnFirstFailure()
+        // true if validation pass method
+        ->passes();
+    }
+
+
+    public function test_all_validation()
+    {
+        // check should pass validation
+        $this->assertEquals($this->checkInput([
+            'name' => 'Walalalal',
+            'secret' => 'password123'
+        ]), true);
+
+        // check should not pass validation
+        $this->assertEquals($this->checkInput([
+            'name' => 'Walalala',
+            'secret' => ''
+        ]), false);
+
+        // $this-> other validation
+        // $this-> other validation
+        // $this-> other validation
+        // $this-> other validation
+    }
+
+
+
     /**
      * Test we can get teacher list
      */
@@ -50,4 +90,47 @@ class TeacherTest extends TestCase
             'name' => $teacher['name']
         ]);
     }
+
+    // public function test_add_teacher_name_validation_fail()
+    // {
+    //     $response = $this->postJson('api/teacher', [
+    //         'name' => 12738917289, // suppose string
+    //         'secret' => 'password123'
+    //     ]);
+
+    //     // validation fail code 422
+    //     $response
+    //     ->assertStatus(422)
+    //     ->assertJson([
+    //         "message" => "The given data was invalid.",  
+    //         "errors" => [
+    //             "name" => [
+    //                 "Hey, your name is UN acceptable !!"
+    //             ]
+    //         ]
+    //     ]);
+    // }
+
+    // public function test_add_teacher_password_fail()
+    // {
+    //     $response = $this->postJson('api/teacher', [
+    //         'name' => 'Walala bin Ra', // suppose string
+    //         'secret' => ''
+    //     ]);
+
+    //     // validation fail code 422
+    //     $response
+    //     ->assertStatus(422)
+    //     ->assertJson([
+    //         "message" => "The given data was invalid.",  
+    //         "errors" => [
+    //             "secret" => [
+    //                 "The secret field is required."
+    //                 // "The secret must be a string.",
+    //                 // "The secret must only contain letters and numbers.",
+    //                 // "The secret must be at least 5 characters."
+    //             ]
+    //         ]
+    //     ]);
+    // }
 }
